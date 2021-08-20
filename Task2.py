fe24fe9574f561efa32b07f69d1264a0310f7375
Muhammad Ahmad Raza <72725372+AhmadR680@users.noreply.github.com>
2021-08-20 16:22:24 +0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,8 @@
+##
+print("Task2")
+print(".....Internship Program Simple Code Which Can Print My Name....")
+print("So, We are Using Python as a Programming Language.")
+print("And Also PYCHARM as an Interpretor.")
+print("Code Started")
+str='Muhammad Ahmad Raza' ## As name in a String datatype so I declare my name in a String as a str.
+print(str) ## So, Here Simply Print this string
