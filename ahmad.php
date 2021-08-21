55c00c3d22e52482b9988942bd61a5748dbdddc1
Muhammad Ahmad Raza <72725372+AhmadR680@users.noreply.github.com>
2021-08-21 21:28:44 +0500

    Add files via upload

--- ahmad.php
+++ ahmad.php
@@ -1,17 +1,16 @@
 <?php
-//get data from form
+//get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
-$email= $_POST['subject'];
 $message= $_POST['message'];
-$to = "ahmadraza22586@gmail.com";
-$subject = "Mail From Ahmad Resume";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  subject = " . $subject . "\r\n Message =" . $message;
-$headers = "From: noreply@AhmadResume.com" . "\r\n" .
+$to = "youremail@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
-    //redirect
-header("Location:thankyou.html");
 }
+//redirect
+header("Location:thankyou.html");
 ?>
